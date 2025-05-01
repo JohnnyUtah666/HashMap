7cc8d4d4af4beac9952d5a1769e5af8c0d35588e
JohnnyUtah666 <porksloppp@gmail.com>
2025-05-01 09:33:41 -0700

    clean up code

--- script.js
+++ script.js
@@ -105,30 +105,3 @@ class HashTable {
         });
     }
 }
-
-
-const test = new HashTable();
-
-test.set('apple', 'red')
- test.set('banana', 'yellow')
- test.set('carrot', 'orange')
- test.set('dog', 'brown')
- test.set('elephant', 'gray')
- test.set('frog', 'green')
- test.set('grape', 'purple')
- test.set('hat', 'black')
- test.set('ice cream', 'white')
- test.set('jacket', 'blue')
- test.set('kite', 'pink')
- test.set('lion', 'golden')
-
- test.set('moon', 'silver');
-
-//test.clear();
- //console.log(test.keys());
- console.log(test.length());
- test.remove('kite');
- console.log(test.length());
-console.log(test.has('kite'));
-
- 
